744b8c20ca8b984a70c0684e7c394edc2b9b2d7c
znueni <49023230+znueni@users.noreply.github.com>
2025-11-05 14:47:16 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.web = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.web, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'web
+        '
+        Me.web.AllowExternalDrop = True
+        Me.web.CreationProperties = Nothing
+        Me.web.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.web.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.web.Location = New System.Drawing.Point(0, 0)
+        Me.web.Name = "web"
+        Me.web.Size = New System.Drawing.Size(800, 450)
+        Me.web.TabIndex = 0
+        Me.web.ZoomFactor = 1.0R
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.web)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.web, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents web As Microsoft.Web.WebView2.WinForms.WebView2
+End Class
---
+++ Form1.vb
@@ -0,0 +1,75 @@
+﻿Imports Microsoft.Web.WebView2.Core
+
+Public Class Form1
+
+    Public Sub New()
+        Application.EnableVisualStyles()
+
+        InitializeComponent()
+
+        Dim webview_environment As CoreWebView2Environment
+        Dim msedgewebview2Folder As String = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86)
+
+        Dim user_data_folder As String = IO.Path.Combine(IO.Path.GetTempPath(), System.Reflection.Assembly.GetExecutingAssembly().GetName.Name)
+        If IO.Directory.Exists(user_data_folder) = False Then IO.Directory.CreateDirectory(user_data_folder)
+
+        msedgewebview2Folder = IO.Path.Combine(msedgewebview2Folder, "Microsoft\EdgeWebView\Application\")
+        If IO.Directory.Exists(msedgewebview2Folder) Then
+            For Each subs As IO.DirectoryInfo In New IO.DirectoryInfo(msedgewebview2Folder).GetDirectories()
+                Dim tempv As New Version
+                If Char.IsDigit(subs.Name.Chars(0)) AndAlso Version.TryParse(subs.Name, tempv) Then
+                    Dim t = CoreWebView2Environment.CreateAsync(subs.FullName, user_data_folder)
+                    t.Wait()
+                    webview_environment = t.Result
+                End If
+            Next
+        End If
+
+
+        web.EnsureCoreWebView2Async(webview_environment, Nothing)
+        web.Visible = False
+    End Sub
+
+    Private LoadURI As Uri
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Loading..."
+        Dim args As String() = Environment.GetCommandLineArgs()
+        If args.Length = 2 Then
+            If IO.File.Exists(args(1)) Then
+                If IO.Path.GetExtension(args(1)) = ".url" Then
+                    For Each ll As String In IO.File.ReadAllLines(args(1))
+                        If ll.StartsWith("URL=") Then
+                            LoadURI = New Uri(ll.Substring(4))
+                        ElseIf ll.StartsWith("IconFile=") Then
+                            Me.Icon = New Icon(ll.Substring(9))
+                        End If
+                    Next
+                Else
+                    LoadURI = New Uri("file:///" & args(1))
+                    Me.Icon = System.Drawing.Icon.ExtractAssociatedIcon(args(1))
+                End If
+            Else
+                Try
+                    LoadURI = New Uri(args(1))
+                Catch ex As Exception
+                    MsgBox("Call me with an URI or file as parameter" & vbCrLf & vbCrLf & ex.GetType().Name & ": " & ex.Message, MsgBoxStyle.Exclamation)
+                    Me.Close()
+                    Exit Sub
+                End Try
+            End If
+        Else
+            MsgBox("Call me with an URI or file as parameter", MsgBoxStyle.Exclamation)
+            Me.Close()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub web_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles web.CoreWebView2InitializationCompleted
+        web.Visible = True
+        web.Source = LoadURI
+    End Sub
+
+    Private Sub web_NavigationCompleted(sender As Object, e As CoreWebView2NavigationCompletedEventArgs) Handles web.NavigationCompleted
+        Me.Text = web.CoreWebView2.DocumentTitle
+    End Sub
+End Class
